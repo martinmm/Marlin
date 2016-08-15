74b135ffae778c1f7060d1d514888afb16dffdf2
Martin Mueller <martinmm@pfump.org>
2016-08-15 22:44:46 +0200

    LCD changes for original Hobbyking Fabrikator mini configuration
    as released by chuck / nyx4

--- Marlin/ultralcd_st7920_u8glib_rrd.h
+++ Marlin/ultralcd_st7920_u8glib_rrd.h
@@ -12,9 +12,9 @@
 #define ST7920_DAT_PIN  LCD_PINS_ENABLE
 #define ST7920_CS_PIN   LCD_PINS_RS
 
-//#define PAGE_HEIGHT 8   //128 byte framebuffer
+#define PAGE_HEIGHT 8   //128 byte framebuffer
 //#define PAGE_HEIGHT 16  //256 byte framebuffer
-#define PAGE_HEIGHT 32  //512 byte framebuffer
+//#define PAGE_HEIGHT 32  //512 byte framebuffer
 
 #define WIDTH 128
 #define HEIGHT 64
